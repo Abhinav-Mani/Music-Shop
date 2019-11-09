bc86a8f62e9fb416e3118bb9dfbcfdcf1580d00c
Abhinav Mani <abhinav1946@gmail.com>
2019-11-09 22:57:56 +0530

    Authentication Added

--- app/src/main/java/com/androlord/music_shop/MainActivity.java
+++ app/src/main/java/com/androlord/music_shop/MainActivity.java
@@ -1,14 +1,104 @@
 package com.androlord.music_shop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.androlord.music_shop.Support.Login;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth.AuthStateListener authStateListener;
+    FirebaseAuth mAuth;
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.Logout:
+                signOut();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+        mGoogleSignInClient.signOut();
+    }
+
+    //Button signout,profile;
+    GoogleSignInClient mGoogleSignInClient;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.main_activity_menu,menu);
+        return true;
+    }
+
+    @Override
+    protected void onStart() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        // Build a GoogleSignInClient with the options specified by gso.
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        Log.e("ak47","on Start");
+        super.onStart();
+        Log.e("ak47","on Start after super");
+        mAuth.addAuthStateListener(authStateListener);
+        Log.e("ak47","on Start Ends");
+    }
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+    }
+
+    private void init() {
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        mAuth=FirebaseAuth.getInstance();
+
+
+
+        //startActivity(new Intent(MainActivity.this, Login.class));
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser()==null)
+                {
+                    Log.e("ak47","user null");
+                    Intent intent = new Intent(MainActivity.this, Login.class);
+                    startActivity(intent);
+                }
+                else
+                {
+                    Log.e("ak47","user not null");
+                }
+
+            }
+        };
     }
 }
